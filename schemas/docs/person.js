c5cb9eb5150aa6653bafe80d8f5db7fa309c1815
codesyariah122 <pujiermanto@gmail.com>
2022-05-08 02:21:13 +0700

    sanity.io myportfolio

---
+++ schemas/docs/person.js
@@ -0,0 +1,43 @@
+export default {
+  name: 'person',
+  type: 'document',
+  title: 'Person',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      description: 'Some frontend will require a slug to be set to be able to show the person',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'figure'
+    },
+    {
+      name: 'contactInfo',
+      type: 'contactInfo',
+      title: 'Contact information'
+    },
+    {
+      name: 'bio',
+      title: 'Bio',
+      type: 'bioPortableText'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image'
+    }
+  }
+}
---
+++ schemas/schema.js
@@ -0,0 +1,38 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// docs schema import
+import category from './docs/category'
+import person from './docs/person'
+import sampleProject from './docs/sampleProject'
+
+// object schema
+import bioPortableText from './obj/bioPortableText'
+import figure from './obj/figure'
+import projectMember from './obj/projectMember'
+import contactInfo from './obj/contactInfo'
+import projectPortableText from './obj/projectPortableText'
+import simplePortableText from './obj/simplePortableText'
+
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  // We name our schema
+  name: 'my portfolio',
+  // Then proceed to concatenate our document type
+  // to the ones provided by any plugins that are installed
+  types: schemaTypes.concat([
+    bioPortableText,
+    figure,
+    projectMember,
+    projectPortableText,
+    simplePortableText,
+    contactInfo,
+
+    category,
+    person,
+    sampleProject
+  ]),
+})
